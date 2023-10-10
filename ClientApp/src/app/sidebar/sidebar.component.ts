9a046d4804bb0ab80aeb9ba953307da4d436a63a
returnT0 <vlvolkov25@gmai.com>
2023-10-10 21:01:41 +0200

    routing fix

--- ClientApp/src/app/sidebar/sidebar.component.ts
+++ ClientApp/src/app/sidebar/sidebar.component.ts
@@ -10,7 +10,7 @@ export class SidebarComponent {
   public sidebarLinks = [
     {route: '/home', label: 'Home', imgURL: '/assets/assets/home.svg'},
     {route: '/user', label: 'User', imgURL: '/assets/assets/user.svg'},
-    {route: '/', label: 'Card', imgURL: '/assets/assets/store.svg'},
+    {route: '/card', label: 'Card', imgURL: '/assets/assets/store.svg'},
   ];
 
   constructor(private authService: AuthenticationService) {
