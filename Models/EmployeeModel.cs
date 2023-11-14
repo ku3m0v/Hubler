f056f46c54987f804f0f701979f8af9d6e1920b2
evkuz <st67058@upce.cz>
2023-11-14 21:58:54 +0100

    fix id transition

---
+++ Models/EmployeeModel.cs
@@ -0,0 +1,11 @@
+namespace Hubler.Models;
+
+public class EmployeeModel
+{
+    public String Email { get; set; }
+    public String FirstName { get; set; }
+    public String LastName { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public String SupermarketName { get; set; }
+    public string PhotoBase64 { get; set; }
+}
