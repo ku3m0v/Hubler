31203d3b0f5c0f895df1ad832ad4e5544a57cb06
returnT0 <vlvolkov25@gmail.com>
2023-11-19 01:08:14 +0100

    user: fix display data

--- ClientApp/src/app/user/user.component.ts
+++ ClientApp/src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {FormBuilder} from "@angular/forms";
+import {FormBuilder, FormGroup} from "@angular/forms";
 import {Router} from "@angular/router";
 import {AuthenticationService} from "../service/auth-service/authentication.service";
 import {ProfileData, ProfileService} from "../service/profile-service/profile.service";
@@ -13,6 +13,7 @@ export class UserComponent implements OnInit {
   profile: ProfileData | null = null;
   public isEditing = false;
   error: string | null = null;
+  public profileForm!: FormGroup;
 
 
   @ViewChild('imageInput', {static: false}) imageInput!: ElementRef;
@@ -28,19 +29,70 @@ export class UserComponent implements OnInit {
     return this.authService.isUserSignedIn();
   }
 
-  ngOnInit(): void {
-    this.loadProfile();
+    ngOnInit(): void {
+        this.profileForm = this.fb.group({
+            email: [{value: '', disabled: true}],  // Assuming email is not editable
+            firstName: [''],
+            lastName: [''],
+            createdDate: [{value: '', disabled: true}], // Assuming date is not editable
+            supermarketName: ['']
+        });
+        this.loadProfile();
+    }
+
+
+  toggleEdit() {
+    this.isEditing = !this.isEditing;
   }
 
-  loadProfile(): void {
-    this.profileService.getProfile().subscribe({
-      next: (data) => {
-        this.profile = data;
-      },
-      error: (err) => {
-        this.error = err.message;
-      }
-    });
+  saveProfile() {
+    if (this.profileForm.valid) {
+      this.toggleEdit();
+    }
   }
+
+  cancelEdit() {
+    this.profileForm.reset(this.profileForm.value);
+    this.toggleEdit();
+  }
+
+  changeImage() {
+    this.imageInput.nativeElement.click();
+  }
+
+  onImageChange(event: Event) {
+    const file = (event.target as HTMLInputElement).files![0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const imageElement: HTMLImageElement = document.querySelector('img[alt="Profile picture"]') as HTMLImageElement;
+        imageElement.src = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+    loadProfile(): void {
+        this.profileService.getProfile().subscribe({
+            next: (data) => {
+                this.profile = data;
+                this.profileForm.setValue({
+                    email: data.email,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    createdDate: this.formatDate(data.createdDate),
+                    supermarketName: data.supermarketName
+                });
+            },
+            error: (err) => {
+                this.error = err.message;
+            }
+        });
+    }
+
+    private formatDate(dateString: Date): string {
+        const date = new Date(dateString);
+        return date.toISOString().split('T')[0];
+    }
 
 }
