bda9ebd4f55c2ef930ca05c6c21b09a331f04bd9
evkuz <st67058@upce.cz>
2023-12-01 18:48:05 +0100

    fix views

--- Controllers/ViewsController.cs
+++ Controllers/ViewsController.cs
@@ -8,48 +8,48 @@ namespace Hubler.Controllers;
 [Route("api/view")]
 public class ViewsController : ControllerBase
 {
-    private readonly IViewDAL<ExpiredInventory> _expiredInventoryDAL;
-    private readonly IViewDAL<ExpiredWarehouse> _expiredWarehouseDAL;
-    private readonly IViewDAL<Top5ProductsBySupermarket> _top5ProductsBySupermarketDAL;
-    private readonly IViewDAL<SupermarketSalesSummary> _supermarketSalesSummaryDAL;
+    private readonly IViewDAL<ExpiredInventory> _expiredInventoryDal;
+    private readonly IViewDAL<ExpiredWarehouse> _expiredWarehouseDal;
+    private readonly IViewDAL<Top5ProductsBySupermarket> _top5ProductsBySupermarketDal;
+    private readonly IViewDAL<SupermarketSalesSummary> _supermarketSalesSummaryDal;
 
     public ViewsController(
-        IViewDAL<ExpiredInventory> expiredInventoryDAL,
-        IViewDAL<ExpiredWarehouse> expiredWarehouseDAL,
-        IViewDAL<Top5ProductsBySupermarket> top5ProductsBySupermarketDAL,
-        IViewDAL<SupermarketSalesSummary> supermarketSalesSummaryDAL)
+        IViewDAL<ExpiredInventory> expiredInventoryDal,
+        IViewDAL<ExpiredWarehouse> expiredWarehouseDal,
+        IViewDAL<Top5ProductsBySupermarket> top5ProductsBySupermarketDal,
+        IViewDAL<SupermarketSalesSummary> supermarketSalesSummaryDal)
     {
-        _expiredInventoryDAL = expiredInventoryDAL;
-        _expiredWarehouseDAL = expiredWarehouseDAL;
-        _top5ProductsBySupermarketDAL = top5ProductsBySupermarketDAL;
-        _supermarketSalesSummaryDAL = supermarketSalesSummaryDAL;
+        _expiredInventoryDal = expiredInventoryDal;
+        _expiredWarehouseDal = expiredWarehouseDal;
+        _top5ProductsBySupermarketDal = top5ProductsBySupermarketDal;
+        _supermarketSalesSummaryDal = supermarketSalesSummaryDal;
     }
 
     // GET: api/view/ExpiredInventory
     [HttpGet("ExpiredInventory")]
     public ActionResult<IEnumerable<ExpiredInventory>> GetExpiredInventory()
     {
-        return Ok(_expiredInventoryDAL.GetAll());
+        return Ok(_expiredInventoryDal.GetAll());
     }
 
     // GET: api/view/ExpiredWarehouse
     [HttpGet("ExpiredWarehouse")]
     public ActionResult<IEnumerable<ExpiredWarehouse>> GetExpiredWarehouse()
     {
-        return Ok(_expiredWarehouseDAL.GetAll());
+        return Ok(_expiredWarehouseDal.GetAll());
     }
 
     // GET: api/view/Top5ProductsBySupermarket
     [HttpGet("Top5ProductsBySupermarket")]
     public ActionResult<IEnumerable<Top5ProductsBySupermarket>> GetTop5ProductsBySupermarket()
     {
-        return Ok(_top5ProductsBySupermarketDAL.GetAll());
+        return Ok(_top5ProductsBySupermarketDal.GetAll());
     }
 
     // GET: api/view/SupermarketSalesSummary
     [HttpGet("SupermarketSalesSummary")]
     public ActionResult<IEnumerable<SupermarketSalesSummary>> GetSupermarketSalesSummary()
     {
-        return Ok(_supermarketSalesSummaryDAL.GetAll());
+        return Ok(_supermarketSalesSummaryDal.GetAll());
     }
 }
