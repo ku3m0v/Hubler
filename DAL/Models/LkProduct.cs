c4bb59140037c8ad563091d0df299029275a6cb9
evkuz <st67058@upce.cz>
2023-12-02 20:33:48 +0100

    LkProduct: add DAL and controller

---
+++ DAL/Models/LkProduct.cs
@@ -0,0 +1,8 @@
+﻿namespace Hubler.DAL.Models;
+
+public class LkProduct
+{
+    public int LkProductId { get; set; }
+    public string Title { get; set; }
+    public decimal CurrentPrice { get; set; }
+}
