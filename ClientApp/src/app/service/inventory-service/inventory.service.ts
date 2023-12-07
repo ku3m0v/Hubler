ad63a87a1b5658803effff2a9c5279d0650dcedc
returnT0 <vlvolkov25@gmail.com>
2023-12-07 14:42:34 +0100

    Service: added inventory service

---
+++ ClientApp/src/app/service/inventory-service/inventory.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { AuthenticationService } from '../auth-service/authentication.service'; // Update the path accordingly
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import configurl from '../../../assets/config/config.json';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InventoryService {
+  private myAppUrl: string = configurl.apiServer.url;
+
+  constructor(private http: HttpClient, private authService: AuthenticationService) {}
+
+  // Get all inventory items
+  getAllInventory(): Observable<InventoryModel[]> {
+    return this.http.get<InventoryModel[]>(`${this.myAppUrl}api/inventory/list`, { headers: this.createHeaders() })
+      .pipe(catchError(this.errorHandler));
+  }
+
+  // Delete an inventory item
+  deleteInventory(id: number): Observable<any> {
+    return this.http.delete(`${this.myAppUrl}api/inventory/delete?id=${id}`, { headers: this.createHeaders() })
+      .pipe(catchError(this.errorHandler));
+  }
+
+  // Order products
+  orderProducts(): Observable<any> {
+    return this.http.post(`${this.myAppUrl}api/inventory/order_products`, {}, { headers: this.createHeaders() })
+      .pipe(catchError(this.errorHandler));
+  }
+
+  private createHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Authorization': `Bearer ${this.authService.getToken()}`
+    });
+  }
+
+  private errorHandler(error: HttpErrorResponse) {
+    console.error('An error occurred:', error.message);
+    return throwError(error);
+  }
+}
+
+export interface InventoryModel {
+  id: number;
+  supermarketTitle: string;
+  productId: number;
+  quantity: number;
+  title: string;
+  currentPrice: number;
+  productType: string;
+  expiryDate?: Date;
+  storageType?: string;
+  shelfLife?: number;
+}
