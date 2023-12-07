15e95305fc71a51667ba9ca67942318dbfb52b39
ku3m0v <st67058@upce.cz>
2023-12-07 13:51:46 +0100

    Views: fix getAll data

--- DAL/Models/ExpiredWarehouse.cs
+++ DAL/Models/ExpiredWarehouse.cs
@@ -2,11 +2,11 @@
 
 public class ExpiredWarehouse
 {
-    public int WarehouseId { get; set; }
+    public int Warehouse_Id { get; set; }
     public int ProductId { get; set; }
-    public string ProductTitle { get; set; }
+    public string Product_Title { get; set; }
     public int Quantity { get; set; }
     public int SupermarketId { get; set; }
-    public string SupermarketTitle { get; set; }
+    public string Supermarket_Title { get; set; }
     public DateTime ExpiryDate { get; set; }
 }
